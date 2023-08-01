74e0b79cf40b092f113c340275363db69e87ea3a
Aslam Ahmed <highflyingaslam@gmail.com>
2023-08-01 21:14:20 +0530

    first git commit

---
+++ src/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <p>djkjfkdjfjdk</p>
+      <p className="yellow">djkjfkdjfjdk</p>
+    </div>
+  );
+};
+
+export default Home;
